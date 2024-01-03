61fd3205d3c439df5ade2f7d6ed08bffda7a3e5c
Boyu Gou <geminigby@gmail.com>
2024-01-02 22:50:17 -0500

    format code

--- src/offline_experiment.py
+++ src/offline_experiment.py
@@ -1,25 +1,21 @@
 ﻿from data_utils.prompts import generate_prompt
-import json,jsonlines
+import json, jsonlines
 import os
 from utils.gpt4v_api import OpenaiEngine
 
-
 generation_model = OpenaiEngine(
-        rate_limit=-1,
-        api_key="Your API Key",
-    )
+    rate_limit=-1,
+    api_key="Your API Key",
+)
 
-exp_split="4api"
+exp_split = "4api"
 source_data_path = "../data/examples/exp4"
-
 
 for action_file in os.listdir(source_data_path):
     if action_file.startswith('.') or not os.path.isdir(os.path.join(source_data_path, action_file)):
         continue
 
-
     print(f"Start testing: {action_file}")
-
 
     if os.path.exists(os.path.join(source_data_path, action_file,
                                    f'prediction-{exp_split}.jsonl')):
@@ -29,21 +25,22 @@ for action_file in os.listdir(source_data_path):
     with open(os.path.join(source_data_path, action_file, "queries.jsonl")) as reader:
         for obj in reader:
             query_meta_data.append(json.loads(obj))
-    predictions=[]
+    predictions = []
     for query_id, query in enumerate(query_meta_data):
-        print("-"*10)
+        print("-" * 10)
         print(os.path.splitext(os.path.basename(action_file))[0] + "-" + str(query_id))
         image_path = query['image_path'] + "/" + str(query_id) + ".jpg"
         image_path = image_path.replace('../', '')
         image_path = image_path.replace('./', '')
-        image_path=source_data_path+"/"+image_path
-        choices_input=None
+        image_path = source_data_path + "/" + image_path
+        choices_input = None
         try:
-            choices_input=query['choices']
+            choices_input = query['choices']
         except:
             pass
-        prompt_list=generate_prompt(exp_split,task=query['confirmed_task'],previous=query['previous_actions'],choices=choices_input)
-        print("-"*10)
+        prompt_list = generate_prompt(exp_split, task=query['confirmed_task'], previous=query['previous_actions'],
+                                      choices=choices_input)
+        print("-" * 10)
         print(prompt_list[0])
         print(prompt_list[1])
 
@@ -66,11 +63,10 @@ for action_file in os.listdir(source_data_path):
         print("#" * 10)
         print(output1)
 
-        output_list=[output0,output1]
+        output_list = [output0, output1]
         output_jsonl = dict(multichoice_id=query_id, gpt_output=output_list, prompt=prompt_list)
         predictions.append(output_jsonl)
     with jsonlines.open(
             os.path.join(source_data_path, action_file, f"prediction-{exp_split}.jsonl"),
             mode='w') as writer:
         writer.write_all(predictions)
-
