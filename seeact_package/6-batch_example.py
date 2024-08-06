3e6c61dd28c1544f63e91fafea34be93d3e712a9
Zheng Du <deandz@gmail.com>
2024-08-05 22:34:26 -0400

    batch running

---
+++ seeact_package/6-batch_example.py
@@ -0,0 +1,38 @@
+import asyncio
+import os
+from seeact.agent import SeeActAgent
+import json
+
+# Setup your API Key here, or pass through environment
+# os.environ["OPENAI_API_KEY"] = "Your API KEY Here"
+# os.environ["GEMINI_API_KEY"] = "Your API KEY Here"
+
+batch = [
+    "ff8cc274-30e0-40b2-86af-29360d548351"
+]
+
+async def run_agent(task_id, website, confirmed_task, grounding):
+    agent = SeeActAgent(
+        model="gpt-4o",
+        default_task=confirmed_task,
+        default_website=website,
+        save_file_dir=f"{task_id}_{grounding}",
+        grounding_strategy=grounding,
+        max_auto_op=30,
+        max_continuous_no_op=3,
+    )
+    await agent.start()
+    while not agent.complete_flag:
+        prediction_dict = await agent.predict()
+        await agent.execute(prediction_dict)
+    await agent.stop()
+
+if __name__ == "__main__":
+    with open("../data/online_tasks/merged_online_90_tasks.json", "r") as file:
+        tasks = json.load(file)
+
+    for task in tasks:
+        if task["task_id"] in batch:
+            print(f"Performing task: {task}")
+            # asyncio.run(run_agent(grounding="text_choice_som", task_id=task["task_id"], website=task["website"], confirmed_task=task["confirmed_task"]))
+            asyncio.run(run_agent(grounding="text_choice", task_id=task["task_id"], website=task["website"], confirmed_task=task["confirmed_task"]))
